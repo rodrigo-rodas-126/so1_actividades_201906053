1f08276c9289af5adb6b89992500bc53e2c85cd9
rodrigo <rodrigito49@gmail.com>
2024-03-03 21:34:26 -0600

    Paths

--- actividad4/script.sh
+++ actividad4/script.sh
@@ -11,13 +11,13 @@ do
     read -p "Mensaje del P1: " primer_mensaje
     echo "$primer_mensaje" > ./mensajes_participante1
 
-    message_recibido_1=$(cat /tmp/mensajes_participante1)
+    message_recibido_1=$(cat ./mensajes_participante1)
     echo "Mensaje Recibido de P1 $message_recibido_1"
 
 
     read -p "Mensaje del P2: " segundo_mensaje
     echo "$segundo_mensaje" > ./mensajes_participante2
 
-    message_recibido_2=$(cat /tmp/mensajes_participante2)
+    message_recibido_2=$(cat ./mensajes_participante2)
     echo "Mensaje Recibido de P2 $message_recibido_2"
 done
